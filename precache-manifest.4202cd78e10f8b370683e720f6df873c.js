189c44385960d25a246941622efd490ad8562a29
Axel <axosar@gmail.com>
2018-11-07 11:14:08 -0600

    doug-score
    Test

---
+++ precache-manifest.4202cd78e10f8b370683e720f6df873c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24f50fe22ea356bdeaaa",
+    "url": "/static/js/main.24f50fe2.chunk.js"
+  },
+  {
+    "revision": "8c0859d6a58d61cc3a39",
+    "url": "/static/js/1.8c0859d6.chunk.js"
+  },
+  {
+    "revision": "24f50fe22ea356bdeaaa",
+    "url": "/static/css/main.196229ca.chunk.css"
+  },
+  {
+    "revision": "0f90e843e799d39788e3a8c27bd3d9c4",
+    "url": "/index.html"
+  }
+];
